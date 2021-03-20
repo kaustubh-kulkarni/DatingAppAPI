0594c4ee65ef34b550455f342748f3b043275da8
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-20 14:35:34 +0100

    Created first entity AppUser.cs and added properties

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,13 @@
+namespace API.Entities
+{
+    // User class for application
+    public class AppUser
+    {
+        // Using shorthand version to get and set from other classes
+        // Property for ID
+        public int Id { get; set; }
+        // Property for Username
+        public string UserName { get; set; }
+        
+    }
+}
