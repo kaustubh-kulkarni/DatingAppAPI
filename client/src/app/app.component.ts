3c49bddee343ebd8bd0708b46b4da70cdaebce2c
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-22 11:31:20 +0100

    Added few extensions and updated files

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+// decorator
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client';
+  title = 'The Dating App';
 }
