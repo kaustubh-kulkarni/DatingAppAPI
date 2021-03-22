01ad0a1a668ff1b3e2b7d13be866733ec0701f6f
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-22 11:45:16 +0100

    Created first method in app component and used http client to send request to api

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 // decorator
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'The Dating App';
+  // Users are type of any i.e we are turning of type safety
+  users: any;
+  // Dependency injection
+  constructor(private http: HttpClient) {}
+
+  // Lifecycle events after constructor are called initializers
+  ngOnInit(){
+    this.getUsers();
+  }
+
+  getUsers(){
+    // http get request should consist of url and will get response in observables
+    // we need to subscribe to observables to get the data
+    this.http.get('https://localhost:5001/api/users').subscribe(response => {
+      this.users = response;
+    }, error => {
+      console.log(error);
+    })
+  }
+
 }
