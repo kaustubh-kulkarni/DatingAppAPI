2106afd7a576db437777cead9447dfec286b78c4
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-25 13:09:03 +0100

    Updated the code in nav.ts and nav.html

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -8,19 +10,17 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {}
-  loggedIn: boolean;
   // Inject accountService in this constructor
-  constructor(private accountService: AccountService) { }
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
-    this.getCurrentUser();
+    this.accountService.currentUser$;
   }
 
   // Login method
   login(){
     this.accountService.login(this.model).subscribe(response => {
       console.log(response);
-      this.loggedIn = true;
     }, error => {
       console.log(error);
     });
@@ -28,16 +28,6 @@ export class NavComponent implements OnInit {
 
   logout(){
     this.accountService.logout();
-    this.loggedIn = false;
-  }
-
-  getCurrentUser(){
-    this.accountService.currentUser$.subscribe(user => {
-      // !! turns our obj into boolean
-      this.loggedIn = !!user;
-    }, error => {
-      console.log(error);
-    })
   }
 
 }
