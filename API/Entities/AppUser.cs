2989239b91e9107687000f67a2afc40ff31c67ae
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-30 10:23:53 +0200

    Extened AppUser with some entities and added photo class for photos

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace API.Entities
 {
     // User class for application
@@ -11,6 +14,17 @@ namespace API.Entities
         // Property for passwords
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime LastActive { get; set; } = DateTime.Now;
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
         
     }
 }
