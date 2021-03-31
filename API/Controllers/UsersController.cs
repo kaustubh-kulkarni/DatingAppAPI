d933867ca16faa099d73eb2b94b9386b4f1c4ce1
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-03-31 17:48:39 +0200

    Updated UserRepo and UsersController

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -13,25 +14,27 @@ namespace API.Controllers
     [Authorize]
     public class UsersController : BaseApiController
     {
-        private readonly DataContext _context;
-        public UsersController(DataContext context)
+        private readonly IUserRepository _userRepository;
+        public UsersController(IUserRepository userRepository)
         {
-            _context = context;
+            _userRepository = userRepository;
         }
         // Get all the users
         // always make DB call async to process data requests
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
             // It goes to datacontext class, access the user table and then access the data inside
             // ToListAsync is async method comes from EF Core
-            return await _context.Users.ToListAsync();
-            
+            return Ok(await _userRepository.GetUsersAsync());
+
         }
         //  Get users with id's with api/users/id
-        [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetUser(int id){
+        [HttpGet("{username}")]
+        public async Task<ActionResult<AppUser>> GetUser(string username)
+        {
             // It goest to datacontext class, access the user table and then access the data inside
-            return await _context.Users.FindAsync(id);
+            return await _userRepository.GetUserByUsernameAsync(username);
         }
     }
 }
