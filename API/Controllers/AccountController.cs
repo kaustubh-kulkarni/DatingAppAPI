93bb4600a38896b4b2201673285566b5b084afcb
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-04-07 10:09:01 +0200

    Added photo thumbnail near login user

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -55,7 +56,7 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+            var user = await _context.Users.Include(p => p.Photos).SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
             // Check if the user is empty or not
             if (user == null) return Unauthorized("Invalid Username");
             // reverse the computed hash
@@ -69,7 +70,8 @@ namespace API.Controllers
              return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
 
 
