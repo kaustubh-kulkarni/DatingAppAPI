f967ea57efbc52eaeb5f51e068894110bbed07fe
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-04-07 13:59:42 +0200

    Created component for reusable text input

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -26,6 +26,7 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
+import { TextInputComponent } from './_forms/text-input/text-input.component';
 
 
 
@@ -44,7 +45,8 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     ServerErrorComponent,
     MemberCardComponent,
     MemberEditComponent,
-    PhotoEditorComponent
+    PhotoEditorComponent,
+    TextInputComponent
   ],
   imports: [
     BrowserModule,
